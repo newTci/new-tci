766ccb77be02316c4415e4b7b60231f309a75167
squalvj <squalvj@gmail.com>
2017-05-15 18:07:05 +0700

    nambahin section pertama dan benerin fixed banner

--- application/views/index.php
+++ application/views/index.php
@@ -9,9 +9,67 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 		<title>Topcareer Indonesia</title>
 		<link href="<?php echo base_url(); ?>assets/bower_components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
 		<link href="<?php echo base_url(); ?>assets/css/main.css" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet">
 	</head>
 	<body>
-		<h1>Hello, world!</h1>
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-sm-2 relative">
+					<div class="left-side-banner hidden-sm">
+						<a href="#">
+							<img src="<?php echo base_url(); ?>assets/gambar/serv/banner.jpg">
+						</a>
+					</div>
+				</div>
+				<!-- Content Center -->
+				<div class="col-sm-8 no-gutter">
+					<div class="wrapper-center">
+						<div class="logo">
+							<img src="" alt="">
+						</div>
+						<div class="first-wrapper">
+							<div id="first-sub-left" class="col-sm-2 no-gutter">
+								<div class="wrapper-box">
+									<h3>TEST</h3>
+								</div>
+
+								<div class="wrapper-box">
+									<h3>TEST</h3>
+								</div>
+
+								<div class="wrapper-box">
+									<h3>TEST</h3>
+								</div>
+							</div>
+							<div class="col-sm-8">
+								<img id="first-img" >
+							</div>
+							<div id="first-sub-right" class="col-sm-2 no-gutter">
+								<div class="wrapper-box">
+									<h3>TEST</h3>
+								</div>
+
+								<div class="wrapper-box">
+									<h3>TEST</h3>
+								</div>
+
+								<div class="wrapper-box">
+									<h3>TEST</h3>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<!-- /Content Center -->
+				<div class="col-sm-2">
+					<div class="right-side-banner  hidden-sm">
+						<a href="#">
+							<img src="<?php echo base_url(); ?>assets/gambar/serv/banner.jpg">
+						</a>
+					</div>
+				</div>
+			</div>
+		</div>	
 		
 		<!-- javascript -->
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
