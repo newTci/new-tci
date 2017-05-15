f3a23498e6eb24b497605ba694cf4a6834a923b5
squalvj <squalvj@gmail.com>
2017-05-15 09:50:32 +0700

    new init

---
+++ application/views/index.php
@@ -0,0 +1,21 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Topcareer Indonesia</title>
+		<link href="<?php echo base_url(); ?>assets/bower_components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+		<link href="<?php echo base_url(); ?>assets/css/main.css" rel="stylesheet">
+	</head>
+	<body>
+		<h1>Hello, world!</h1>
+		
+		<!-- javascript -->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+		<script src="<?php echo base_url(); ?>assets/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+		<script src="<?php echo base_url(); ?>assets/js/js.js"></script>
+	</body>
+</html>
