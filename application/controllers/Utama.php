597f38c2819038018091d4b15141df830275a3e0
squalvj <squalvj@gmail.com>
2017-05-19 10:23:05 +0700

    sebelum dipisah template

--- application/controllers/Utama.php
+++ application/controllers/Utama.php
@@ -12,4 +12,9 @@ class Utama extends CI_Controller {
 	{
 		$this->load->view('index');
 	}
+
+	public function berita()
+	{
+		$this->load->view('template/header');
+	}
 }
---
+++ application/views/template/header.php
@@ -0,0 +1,34 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Topcareer Indonesia</title>
+		<link href="<?php echo base_url(); ?>assets/bower_components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+		<link href="<?php echo base_url(); ?>assets/css/main.css" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/css?family=Ubuntu|Montserrat|Roboto" rel="stylesheet">
+		<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/bower_components/owl.carousel/dist/assets/owl.carousel.min.css">
+		<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/bower_components/owl.carousel/dist/assets/owl.theme.default.min.css">
+		<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+	</head>
+	<body>
+		<div id="utama" class="container">
+			<div class="background-container"></div>
+			<div class="row">
+				<div class="col-sm-2 col-lg-1 relative">
+					<div class="left-side-banner hidden-sm">
+						<a href="#">
+							<img class="side-banner" src="<?php echo base_url(); ?>assets/gambar/serv/banner.jpg">
+						</a>
+					</div>
+				</div>
+				<!-- Content Center -->
+				<div class="col-sm-8 no-gutter col-lg-10">
+					
+					
+				</div>
+				<!-- /Content Center -->
+				
