c8ecbc5b858f9d79463e6b0670bebb29c8fe186d
squalvj <squalvj@gmail.com>
2017-05-26 15:12:05 +0700

    fix responsive di section berita detail

---
+++ application/views/podcast/index.php
@@ -0,0 +1,40 @@
+<div class="col-sm-8 no-gutter col-lg-10">
+	<div class="big-divider">
+		<span class="text-divider">TOPCAREER TV PODCAST</span>
+	</div>
+	<div class="row no-margin">
+		<div class="col-sm-2 text-left no-gutter">
+			<span class="interaksi-side-text">Trending</span>
+		</div>
+		<div class="col-sm-2 col-sm-offset-8 text-right no-gutter">
+			<span class="interaksi-side-text">Rekomen</span>
+		</div>
+	</div>
+	<div class="row no-margin">
+		<div class="col-lg-2 col-sm-2 col-md-2 no-gutter">
+			<div class="wrapper-box">
+				<span class="hashtag">#ahok</span>
+			</div>
+			<div class="wrapper-box">
+				<span class="hashtag">#ahok</span>
+			</div>
+			<div class="wrapper-box">
+				<span class="hashtag">#ahok</span>
+			</div>
+		</div>
+		<div class="col-lg-8 col-sm-8 col-md-8 no-gutter">
+			
+		</div>
+		<div class="col-lg-2 col-sm-2 col-md-2 no-gutter">
+			<div class="wrapper-box">
+				<span class="hashtag">#ahok</span>
+			</div>
+			<div class="wrapper-box">
+				<span class="hashtag">#ahok</span>
+			</div>
+			<div class="wrapper-box">
+				<span class="hashtag">#ahok</span>
+			</div>
+		</div>
+	</div>
+</div>
